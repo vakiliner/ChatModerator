13cc8ba21966b5814b2d308c9b82bce5527154ca
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2025-11-24 20:25:22 +0300

    refactor: Перенос кода spectatorsChat для Fabric

--- vakiliner/chatcomponentapi/base/ChatPlayer.java
+++ vakiliner/chatcomponentapi/base/ChatPlayer.java
@@ -1,8 +1,11 @@
 package vakiliner.chatcomponentapi.base;
 
 import vakiliner.chatcomponentapi.common.ChatGameMode;
+import vakiliner.chatcomponentapi.component.ChatComponent;
 
 public interface ChatPlayer extends ChatOfflinePlayer, ChatCommandSender {
+	ChatComponent getDisplayName();
+
 	ChatGameMode getGameMode();
 
 	default boolean isConsole() {
